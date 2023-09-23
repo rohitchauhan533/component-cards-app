d9110f07b1fcec18bb0bb6fae358db5769185706
rohitchauhan533 <chauhanrohit533@gmail.com>
2023-09-23 23:30:38 +0530

    initialing delete functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+const sports = ["basketball", "football", "cricket"];
 
 function App() {
-  return (
-    <div className="App">
+  // Initialize the state with the initial value
+  const [arrCopy, setArrCopy] = useState(sports);
+  const [check, setCheck] = useState(false)
 
+  const handleCheckbox = (index) => {
+     console.log("it is check", index)
+  }
+
+  const handleDelete = (index) => {
+    const filterCopy = arrCopy.filter((item, i) => i !== index);
+    setArrCopy(filterCopy);
+  }
+
+  return (
+    <div>
+      <ul>
+        {arrCopy.map((item, i) => (
+          <li key={i}>
+            <input type ="checkbox" onChange={(e,i)=> handleCheckbox(e.target.value, i)}></input>
+            {item}
+            <button onClick={() => handleDelete(i)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
